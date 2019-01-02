9bc283a4871775104dfa845cde5da8091cb2e1d4
Jon Freer <j.freer@hotmail.co.uk>
2019-01-02 00:06:21 +0000

    Added horizontal wipe

---
+++ Horizontal-wipe/casparcg.js
@@ -0,0 +1,33 @@
+array={}
+
+//This function is designed to update any information and get the animation ready to play
+function update(arg) { //the key/value pairs configured in CasparCG client are passed here
+ $(arg).find('componentData').each(function(i,j)
+    {         
+        array[$(j).attr("id")]=$(j).find("data").attr("value")
+       
+    });
+    if (array["f0"] !== undefined) {
+        $("#text-element").html(array["f0"]);
+    }
+    if (array["f1"] !== undefined) {
+        $("#text-element2").html(array["f1"]);
+    }
+}
+
+
+function play(arg) {
+    $(".bars").addClass("added")
+    $(".text1").addClass("added2")
+    $(".text2").addClass("added2")
+}
+
+function stop() {
+    $(".bars").removeClass("added")
+    $(".text1").removeClass("added2")
+    $(".text2").removeClass("added2")
+}
+
+function next() {
+
+}
