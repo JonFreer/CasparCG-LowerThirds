3b866d1f09e1179c108911f277e98c38eb81c534
Jon Freer <j.freer@hotmail.co.uk>
2019-01-15 20:03:27 +0000

    Added 3D Swing

---
+++ 3D-Swing/casparcg.js
@@ -0,0 +1,37 @@
+array={}
+
+//This function is designed to update any information and get the animation ready to play
+function update(arg) { //the key/value pairs configured in CasparCG client are passed here
+ $(arg).find('componentData').each(function(i,j)
+    {         
+        array[$(j).attr("id")]=$(j).find("data").attr("value")
+       
+    });
+    if (array["f0"] !== undefined) {
+        $("#text-element").html(array["f0"]);
+    }
+    if (array["f1"] !== undefined) {
+        $("#text-element2").html(array["f1"]);
+    }
+    if (array["f2"] !== undefined) {
+        $("body").get(0).style.setProperty("--back-color", array["f2"]);
+    }
+    console.log(array["f3"])
+    if (array["f3"] !== undefined) {
+        $("body").get(0).style.setProperty("--scale", array["f3"]);
+    }
+}
+
+
+function play(arg) {
+    $(".swing").addClass("swing-added")
+
+}
+
+function stop() {
+    $(".swing").removeClass("swing-added")
+}
+
+function next() {
+
+}
